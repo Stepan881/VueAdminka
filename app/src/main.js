7b87d136ced44248de4004fb9ac491b5a830ffff
stepan881 <stepanstepanov180@gmail.com>
2020-04-29 03:42:59 +0300

    lesson04

--- app/src/main.js
+++ app/src/main.js
@@ -1,28 +1,33 @@
 "use strict";
-
-const $ = require('jquery');
-
-function getPageList() {
-  $("h1").remove();
-  $.get("./api/api.php", (data) => {
-    data.forEach((file) => {
-      $('body').append("<h1>" + file + "</h1>");
-    });
-    
-  }, "JSON"); 
- }
-
-getPageList();
-
-$("button").click(() => {
-
-  $.post("./api/createNewHtmlPage.php", {
-    "name": $("input").val()
-  }, (data) => {
-    getPageList();
-  })
-  .fail(() => {
-    alert('Страница есть! Удалить ее?');  
-
-  });
+// const Vue = require("vue");
+const axios = require("axios");
+new Vue({
+  el: "#app",
+  data: {
+    "pageList": [],
+    "newPageName": "",
+  },
+  methods: {
+    createPage() {
+    axios
+      .post("./api/createNewHtmlPage.php", { "name": this.newPageName })
+      .then(() => this.updatePageList());
+    },
+    updatePageList() {
+          axios
+      .get("./api/")
+      .then((response) => {
+        this.pageList = response.data;
+      });
+    },
+    deletePage(page) {
+      axios 
+      .post("./api/deletePage.php", { "name": page })
+      .then(() => this.updatePageList());
+    }
+  },
+  created() {
+    this.updatePageList();
+  }
 });
+  
